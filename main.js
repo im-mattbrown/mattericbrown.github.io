7b98e0d672f359d92f028710238ed74e33d19a7b
mattericbrown <matt.eric.brown@gmail.com>
2016-10-26 13:07:46 -0700

    fix for Firefox

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+console.log("javascript");
+
 $(document).ready( function(){
     var vid = $('#video')[0];
 
-    video.currentTime = 0;
+    vid.currentTime = 0;
     var step = 0.02;
 
     var scroll = $(window).scrollTop(0);
 
     $(window).bind('mousewheel', function(event) {
-      console.log(scroll);
+
       if (video.currentTime > 0.0 ) {
         $('.down').css("display", "none");
       }
@@ -23,10 +25,31 @@ $(document).ready( function(){
           }
             console.log(window.pageYOffset);
             console.log(video.currentTime);
-            console.log(event.originalEvent.wheelDelta);
+            console.log(window.scrollTop);
         } else{
             video.currentTime-=step;
             console.log(video.currentTime);
         }
     });
+      $(window).bind('MozMousePixelScroll', function(event) {
+
+        console.log(event.originalEvent.detail);
+        if (video.currentTime > 0.0 ) {
+          $('.down').css("display", "none");
+        }
+          if (event.originalEvent.detail > 0) {
+            if (video.currentTime < 5.5) {
+              video.currentTime += Math.sqrt(window.pageYOffset)/2200;
+            } else if (video.currentTime > 5.5 && video.currentTime < 7.5) {
+              video.currentTime += Math.sqrt(window.pageYOffset)/3600;
+            } else if (video.currentTime > 7.5 && video.currentTime < 11.5) {
+              video.currentTime += Math.sqrt(window.pageYOffset)/5000;
+            } else {
+              video.currentTime += Math.sqrt(window.pageYOffset)/5000;
+            }
+          } else{
+              video.currentTime-=step;
+              console.log(video.currentTime);
+          }
+      });
 });
