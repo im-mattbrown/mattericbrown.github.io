276efc7957839be48bee4aa07ef7eb41b68b02eb
mattericbrown <matt.eric.brown@gmail.com>
2016-10-26 15:08:28 -0700

    cleaned up js file

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-console.log("javascript");
-
 $(document).ready( function(){
     var video = $('#video')[0];
 
     video.currentTime = 0;
-    var step = 0.02;
-
-    var scroll = $(window).scrollTop(0);
 
     $(window).bind('mousewheel', function(event) {
 
@@ -23,12 +18,8 @@ $(document).ready( function(){
           } else {
             video.currentTime += Math.sqrt(window.pageYOffset)/5000;
           }
-            console.log(window.pageYOffset);
-            console.log(video.currentTime);
-            console.log(window.scrollTop);
         } else{
             video.currentTime-=step;
-            console.log(video.currentTime);
         }
     });
       $(window).bind('DOMMouseScroll MozMousePixelScroll', function(event) {
@@ -36,7 +27,6 @@ $(document).ready( function(){
           $('.down').css("display", "none");
         }
           if (event.originalEvent.detail > 0) {
-            console.log(video.currentTime);
             if (video.currentTime < 5.5) {
               video.currentTime += Math.sqrt(window.pageYOffset)/2200;
             } else if (video.currentTime > 5.5 && video.currentTime < 7.5) {
@@ -48,7 +38,6 @@ $(document).ready( function(){
             }
           } else{
               video.currentTime-=step;
-              console.log(video.currentTime);
           }
       });
 });
